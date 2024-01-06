63b96c0ad0af9b2f8fc7d5c407451bf6fa9626e9
Bartosz Brodowski <bartolini10058@gmail.com>
2024-01-06 21:46:47 +0100

    feat: hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1>Home</h1>
+		<main className='flex min-h-screen flex-col items-center justify-between px-24'>
+			<Hero />
 		</main>
 	);
 }
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from '../ui/button';
+
+const Hero = () => {
+	return (
+		<div className='grid grid-cols-2 items-center h-screen'>
+			<div className='flex justify-center items-center'>
+				<Image
+					className='rotate-45'
+					width='400'
+					height='500'
+					src='/HeroGuitar.png'
+					alt='Les Paul type guitar drawing'
+				/>
+			</div>
+			<div className='flex flex-col items-start justify-center gap-2'>
+				<h1 className='text-primary-black font-semibold text-6xl break-words'>START STRUMMING</h1>
+				<p className='text-gray-500 mb-4 text-2xl'>Can&apos;t strum without a proper guitar...</p>
+				<Link href='/shop/guitars'>
+					<Button className='w-[200px]' size={'lg'}>
+						Shop now
+					</Button>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
